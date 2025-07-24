fb8644f5a4a4fd6443e3262e020ff4be335d12d8
SteveCoolFart <103543324+SteveCoolFart@users.noreply.github.com>
2025-07-24 17:37:08 -0600

    ye

--- Behaviours/PropDetector.cs
+++ Behaviours/PropDetector.cs
@@ -1,41 +1,41 @@
-﻿using System.Collections.Generic;
-using ExitGames.Client.Photon;
+﻿using System.Collections;
+using System.Collections.Generic;
 using Photon.Pun;
 using Photon.Realtime;
-using UnityEngine;
 using ScoreboardAttributes;
+using UnityEngine;
+using Hashtable = ExitGames.Client.Photon.Hashtable;
 
-namespace MonkeView.Behaviours 
+namespace MonkeView.Behaviours
 {
     public class MonkeView : MonoBehaviourPunCallbacks
     {
-        public override void OnJoinedRoom() => UpdateAllPlayerAttributes();
-        public override void OnPlayerEnteredRoom(Player newPlayer) => UpdatePlayerModsAttribute(newPlayer);
-        public override void OnPlayerPropertiesUpdate(Player player, Hashtable changedProps) => UpdatePlayerModsAttribute(player);
-
-        private void UpdateAllPlayerAttributes() => PhotonNetwork.PlayerList.ForEach(x => UpdatePlayerModsAttribute(x));
-
-        private void UpdatePlayerModsAttribute(Player player)
+        #region Constants
+        static readonly Dictionary<string, string> ModKeyNames = ModKeys.ModNames;
+        static readonly string NoModsText = "<color=red>No Detected Mods</color>";
+        static readonly List<string> buffer = new List<string>(ModKeys.ModNames.Count);
+        #endregion
+        #region Callbacks
+        public override void OnJoinedRoom() => PhotonNetwork.PlayerList.ForEach(p => StartCoroutine(WaitForProps(p)));
+        public override void OnPlayerEnteredRoom(Player p) => StartCoroutine(WaitForProps(p));
+        public override void OnPlayerPropertiesUpdate(Player p, Hashtable _) => StartCoroutine(WaitForProps(p));
+        #endregion
+        #region Actual Functions
+        IEnumerator WaitForProps(Player p)
         {
-            var customProps = player.CustomProperties;
-            var detectedMods = new List<string>();
-
-            var modKeyNamesList = new List<KeyValuePair<string, string>>(ModKeys.ModNames);
-            for (int i = 0; i < modKeyNamesList.Count; i++)
-            {
-                var pair = modKeyNamesList[i];
-                if (customProps.TryGetValue(pair.Key, out var value) && value != null)
-                    detectedMods.Add(pair.Value);
-            }
-
-            Registry.AddAttribute(
-                player,
-                detectedMods.Count > 0
-                    ? string.Join(", ", detectedMods)
-                    : "<color=red>No Detected Mods</color>"
-            );
+            // Steve wrote this, not elliot.
+            // Please yell at SteveTheAnimator on github if you have any issues with this code. (which, you will)
+            yield return new WaitForSeconds(0.25f); // wait for the players to actually, exist
+            for (float t = 0; t < 12f && (p.CustomProperties == null || !HasProps(p)); t += 0.1f) yield return new WaitForSeconds(0.1f); // wait for props to exist
+            if (p.CustomProperties == null) yield break; // if the props are null, break.
+            buffer.Clear(); // clear the buffer
+            foreach (var kv in ModKeyNames) if (p.CustomProperties.TryGetValue(kv.Key, out var v) && v != null) buffer.Add(kv.Value); // if the player has a prop, add it to the buffer
+            VRRig vrRig = null; // boom big the VRRig to null (idk what making a new object called lmao)
+            for (float t = 0; t < 8f && (vrRig = GorillaGameManager.instance?.FindPlayerVRRig(p)) == null; t += 0.1f) yield return new WaitForSeconds(0.1f); // wait for the VRRig not to be null, yes you can scream at me for this.
+            if (vrRig?.OwningNetPlayer != null) Registry.AddAttribute(vrRig.OwningNetPlayer, buffer.Count == 0 ? NoModsText : string.Join(", ", buffer)); // we add the attribute
         }
+
+        static bool HasProps(Player p) => p.CustomProperties?.ContainsKey("didTutorial") == true;
+        #endregion
     }
 }
-
-// if there is a way to improve the code, please do so or i will figure out how to improve it later
