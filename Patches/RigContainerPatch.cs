e9c2eda3654bbb67328a294eeaa903688cbfed9e
elliot! <benjikrolfanpageuwu@gmail.com>
2025-07-25 16:59:19 +0100

    Inital Commit

---
+++ Patches/RigContainerPatch.cs
@@ -0,0 +1,42 @@
+﻿using HarmonyLib;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+using MonkeView.RigHooks;
+
+namespace GorillaInfoWatch.Patches
+{
+    [HarmonyPatch(typeof(RigContainer)), HarmonyWrapSafe]
+    internal class RigContainerPatches
+    {
+        [HarmonyPatch(typeof(RigContainer), "OnRigDisable")]
+        internal class RigContainerPatch
+        {
+            [HarmonyPostfix]
+            public static void DisablePatch(RigContainer __instance)
+            {
+                PhotonView view = __instance.GetComponent<PhotonView>();
+                if (view == null) return;
+
+                Player player = view.Owner;
+                if (player != null)
+                {
+                    RigHooks.OnPlayerRigDestroyed(player);
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(RigContainer), "OnRigDisable")]
+        public static void DisablePatch(RigContainer __instance)
+        {
+            PhotonView view = __instance.GetComponent<PhotonView>();
+            if (view == null) return;
+
+            Player player = view.Owner;
+            if (player != null)
+            {
+                RigHooks.OnPlayerRigDestroyed(player);
+            }
+        }
+    }
+}
---
+++ Patches/RigHooks.cs
@@ -0,0 +1,21 @@
+﻿using Photon.Realtime;
+using ScoreboardAttributes;
+
+namespace MonkeView.RigHooks
+{
+    public static class RigHooks
+    {
+        public static void OnPlayerRigAssigned(Player player)
+        {
+            if (MonkeView.Instance != null)
+            {
+                MonkeView.Instance.UpdatePlayerModsAttribute(player);
+            }
+        }
+
+        public static void OnPlayerRigDestroyed(Player player)
+        {
+            Registry.RemoveAttribute(player);
+        }
+    }
+}
